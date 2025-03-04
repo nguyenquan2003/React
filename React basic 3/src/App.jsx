4169bf14d1ffdcd1fbc1d1d41ef64b2f689a6b3f
Quân <quan22032003@gmail.com>
2025-03-04 20:17:52 +0700

    File upload

---
+++ React basic 3/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+import Card from './Card.jsx'
+import Button from './Button (css theo module)/Button.jsx'
+
+function App() {
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Button/><br/>
+    <Card/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
